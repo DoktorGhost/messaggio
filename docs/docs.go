a4d17029fd96613deb54af5a64e500507d9805c3
Oleg Samsonov <ghostkote@gmail.com>
2024-07-19 13:50:06 +0300

    swag correct

--- docs/docs.go
+++ docs/docs.go
@@ -111,7 +111,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "62.109.17.207:8080",
+	Host:             "localhost:8080",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Messaggio",
